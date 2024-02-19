e9ff3113b4fae2967c29af334a26bd3d39f17c6d
Bablu Mehta <babloomehta895012@gmail.com>
2024-02-19 16:09:31 +0530

    i think i learned the sass/scss all the notes that i learned at moment is in style files as comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import "./styles/app.scss";
 function App() {
   return (
     <>
-      <div>Something</div>
+      <div className="productCard">
+        <h2>sample</h2>
+        <a href="#">sample anchor tag</a>
+        <button>click me</button>
+        <p className="p1">click me also</p>
+        <p className="p2">click me also</p>
+        <p className="p3">click me also</p>
+      </div>
     </>
   );
 }
